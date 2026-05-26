e2e4cb0fb5b808e3ef1e5fd61baf9dfef8db7e64
ark-home <m.vip@qq.com>
2026-05-26 21:04:44 +0800

    短信登录，

--- qianyu-service/qianyu-user-service/src/main/java/com/clmcat/qianyu/user/mapper/UserAuthMapper.java
+++ qianyu-service/qianyu-user-service/src/main/java/com/clmcat/qianyu/user/mapper/UserAuthMapper.java
@@ -3,8 +3,19 @@ package com.clmcat.qianyu.user.mapper;
 import com.clmcat.qianyu.user.model.entity.UserAuth;
 import com.mybatisflex.core.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserAuthMapper extends BaseMapper<UserAuth> {
 
+    @Select("""
+            SELECT id, user_id, identity_type, identifier, credential, create_time, update_time
+            FROM user_auth
+            WHERE identity_type = #{identityType}
+              AND identifier = #{identifier}
+            LIMIT 1
+            """)
+    UserAuth selectByIdentityTypeAndIdentifier(@Param("identityType") String identityType,
+                                               @Param("identifier") String identifier);
 }
